0af8fbcca20f187af1b0527b0baa7202c9b5803e
Behzat Can Acele <61169260+bezata@users.noreply.github.com>
2023-05-04 17:38:11 +0300

    Webpack fixes, contract improvements
    I rearranged your webpack config and index.js also set output for webpack with bundle js. I used old legacy-openssl for npm run dev and build functions because they didn't work before that now with all changes npm functiions just fine with any version on Node so you can deploy it on servers. I made some changes on contract Added a WorkFactory contract that allows for easy creation of Work contracts. The Work contract constructor now takes an argument of the platowner address. We have also added a withdraw function to enable the owner to withdraw any collected fees or funds from the contract. more optimized way of it and added a withdraw function

--- contract/marketplace.sol
+++ contract/marketplace.sol
@@ -3,25 +3,55 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 interface IERC20Token {
-  function transfer(address, uint256) external returns (bool);
-  function approve(address, uint256) external returns (bool);
-  function transferFrom(address, address, uint256) external returns (bool);
-  function totalSupply() external view returns (uint256);
-  function balanceOf(address) external view returns (uint256);
-  function allowance(address, address) external view returns (uint256);
+    function transfer(address, uint256) external returns (bool);
 
-  event Transfer(address indexed from, address indexed to, uint256 value);
-  event Approval(address indexed owner, address indexed spender, uint256 value);
+    function approve(address, uint256) external returns (bool);
+
+    function transferFrom(address, address, uint256) external returns (bool);
+
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address) external view returns (uint256);
+
+    function allowance(address, address) external view returns (uint256);
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
+}
+
+// @dev Added a WorkFactory contract that allows for easy creation of Work contracts. The Work contract constructor now takes an argument of the platowner address. 
+
+contract WorkFactory {
+    address[] public WorkAddresses;
+    mapping(address => bool) internal WorkContracts;
+
+    function createWork() public {
+        address newContract = address(new Work(msg.sender));
+        WorkAddresses.push(newContract);
+        WorkContracts[newContract] = true;
+    }
+
+    function getWorkAddresses() public view returns (address[] memory) {
+        return WorkAddresses;
+    }
+
+    function isWorkContract(address _address) public view returns (bool) {
+        return WorkContracts[_address];
+    }
 }
 
 contract Work {
-
     uint internal Workforce = 0;
     uint internal Assignments = 0;
     uint public platfee;
     address public platowner;
-    
-    address internal cUsdTokenAddress = 0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1;
+
+    address internal cUsdTokenAddress =
+        0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1;
 
     struct Worker {
         address payable Worker;
@@ -42,67 +72,49 @@ contract Work {
         uint price;
         uint index;
         uint SubmissionCounter;
+    }
 
+    mapping(uint => string[]) internal AssignmentCompleted;
+    mapping(uint => mapping(uint => string)) internal submissions;
+    mapping(uint => mapping(uint => address)) internal submitterList;
+    mapping(address => bool) internal workers;
+    mapping(uint => Worker) internal workforce;
+    mapping(uint => Assignment) internal AssignmentsToDo;
+
+    constructor(address _platowner) {
+        platowner = _platowner;
     }
-    
-    mapping (uint => string[]) internal AssignmentCompleted;
-    mapping (uint => mapping(uint => string)) internal submissions;
-    mapping (uint => mapping(uint => address)) internal submitterList;
-    mapping (address => bool) internal workers;
-    mapping (uint => Worker) internal workforce;
-    mapping (uint => Assignment) internal AssignmentsToDo;
-    
-    constructor() {
-        platowner = msg.sender;
-    }
-    
+
     modifier OnlyOwner() {
         require(msg.sender == platowner);
         _;
     }
 
-    modifier OnlyRegistered () {
-        require(workers[msg.sender]);
+    modifier OnlyRegistered() {
+        require(workers[msg.sender], "Worker is not registered.");
         _;
     }
 
-    modifier OnlyEmployer (uint _index) {
-        require(msg.sender == AssignmentsToDo[_index].employer);
+    modifier OnlyEmployer(uint _index) {
+        require(
+            msg.sender == AssignmentsToDo[_index].employer,
+            "Caller is not the employer."
+        );
         _;
     }
 
-    function SetPlatFee (uint _newfee) OnlyOwner() public {
+    function SetPlatFee(uint _newfee) public OnlyOwner {
         platfee = _newfee;
     }
 
-    function Addassignment(
-        string memory _name,
-        string memory _image,
-        string memory _description,  
-        uint _price
-    
-    ) public {
-        require(
-            IERC20Token(cUsdTokenAddress).transferFrom(
-            msg.sender,
-            address(this),
-            _price
-          ),
-          "Transfer failed."
-        );
-
-        AssignmentsToDo[Assignments].employer = payable(msg.sender);
-        AssignmentsToDo[Assignments].name = _name;
-        AssignmentsToDo[Assignments].image= _image;
-        AssignmentsToDo[Assignments].AssignmentDescription = _description;
-        AssignmentsToDo[Assignments].price = _price;
-        AssignmentsToDo[Assignments].index = Assignments;
-        AssignmentsToDo[Assignments].SubmissionCounter = 0;
-        
-        Assignments++;
-    }
-
-    function AddtoWorkforce (
+    /**
+     * @dev Adds a new worker to the contract.
+     * @param _name The name of the worker.
+     * @param _image The image associated with the worker.
+     * @param _description A description of the worker's skills and experience.
+     * AddtoWorkforce function to set the workers mapping to true for a registered worker. This is to ensure that only registered workers can submit work for assignments.
+     */
+    function AddtoWorkforce(
         string calldata _name,
         string calldata _image,
         string calldata _description
@@ -115,51 +127,66 @@ contract Work {
             0,
             Workforce
         );
+        workers[msg.sender] = true;
         Workforce++;
     }
 
-    function GetAssignment(uint _index) OnlyRegistered() public view returns (
-        address payable,
-        string memory, 
-        string memory,  
-        string memory, 
-        uint
-    ) {
+    function GetAssignment(
+        uint _index
+    )
+        public
+        view
+        OnlyRegistered
+        returns (
+            address payable,
+            string memory,
+            string memory,
+            string memory,
+            uint
+        )
+    {
         return (
             AssignmentsToDo[_index].employer,
-            AssignmentsToDo[_index].name, 
-            AssignmentsToDo[_index].image, 
-            AssignmentsToDo[_index].AssignmentDescription, 
+            AssignmentsToDo[_index].name,
+            AssignmentsToDo[_index].image,
+            AssignmentsToDo[_index].AssignmentDescription,
             AssignmentsToDo[_index].price
         );
     }
 
-    function GetWorkerInfo(uint _index) public view returns(
-        address payable,
-        string memory,
-        string memory,
-        string memory,
-        uint
-
-    ) {
-       return(
-          workforce[_index].Worker,
-          workforce[_index].name,
-          workforce[_index].image,
-          workforce[_index].description,
-          workforce[_index].AssignmentsDone 
-       );
+    function GetWorkerInfo(
+        uint _index
+    )
+        public
+        view
+        returns (
+            address payable,
+            string memory,
+            string memory,
+            string memory,
+            uint
+        )
+    {
+        return (
+            workforce[_index].Worker,
+            workforce[_index].name,
+            workforce[_index].image,
+            workforce[_index].description,
+            workforce[_index].AssignmentsDone
+        );
     }
-    
-    function SubmitWork(uint _index, string calldata _submissions) OnlyRegistered() public returns (uint)  {
-        uint SubId = AssignmentsToDo[_index].SubmissionCounter ;
+
+    function SubmitWork(
+        uint _index,
+        string calldata _submissions
+    ) public OnlyRegistered returns (uint) {
+        uint SubId = AssignmentsToDo[_index].SubmissionCounter;
         submissions[_index][SubId] = _submissions;
         submitterList[_index][SubId] = msg.sender;
-        
-        AssignmentsToDo[_index].SubmissionCounter++ ;
 
-        return(SubId);
+        AssignmentsToDo[_index].SubmissionCounter++;
 
+        return (SubId);
     }
 
     /*
@@ -169,7 +196,7 @@ contract Work {
            GetSubmission(_index, i);  
         }
     } */
-    
+
     function getAssignmentsToDoSize() public view returns (uint) {
         return (Assignments);
     }
@@ -182,30 +209,47 @@ contract Work {
         return (Workforce);
     }
 
-    function GetSubmission(uint _index,uint _SubId) public view returns(string memory, address){
-        return(submissions[_index][_SubId],
-               submitterList[_index][_SubId] 
-               );
+    function GetSubmission(
+        uint _index,
+        uint _SubId
+    ) public view returns (string memory, address) {
+        return (submissions[_index][_SubId], submitterList[_index][_SubId]);
     }
 
-    function SetBestSubmission(uint _Aindex, uint _Sindex) public returns(string memory, address){
-         AssignmentsToDo[_Aindex].BestSubmission = submissions[_Aindex][_Sindex];
-         AssignmentsToDo[_Aindex].BestSubmitter = payable(submitterList[_Aindex][_Sindex]);
-         return(
-           AssignmentsToDo[_Aindex].BestSubmission,
-           AssignmentsToDo[_Aindex].BestSubmitter
-         );
+    function SetBestSubmission(
+        uint _Aindex,
+        uint _Sindex
+    ) public returns (string memory, address) {
+        AssignmentsToDo[_Aindex].BestSubmission = submissions[_Aindex][_Sindex];
+        AssignmentsToDo[_Aindex].BestSubmitter = payable(
+            submitterList[_Aindex][_Sindex]
+        );
+        return (
+            AssignmentsToDo[_Aindex].BestSubmission,
+            AssignmentsToDo[_Aindex].BestSubmitter
+        );
     }
 
-    function Award(uint _index) public OnlyEmployer(_index){
+    function Award(uint _index) public OnlyEmployer(_index) {
         require(
             IERC20Token(cUsdTokenAddress).transferFrom(
-            address(this),
-            AssignmentsToDo[_index].BestSubmitter,
-            AssignmentsToDo[_index].price
-          ),
-          "Transfer failed."
+                address(this),
+                AssignmentsToDo[_index].BestSubmitter,
+                AssignmentsToDo[_index].price
+            ),
+            "Transfer failed."
         );
-    
+    }
+
+    /**
+     * @dev Allows the contract owner to withdraw tokens from the contract.
+     * @param _to The address to send the tokens to.
+     * @param _amount The amount of tokens to withdraw.
+     */
+    function withdraw(address _to, uint256 _amount) public OnlyOwner {
+        require(
+            IERC20Token(cUsdTokenAddress).transfer(_to, _amount),
+            "Transfer failed."
+        );
     }
 }
